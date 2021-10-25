eab79ae4d55b95ef6dee9d4a5fe06ac72f41d3c6
Leonardo Silva <81979219+DevSilvaLeo@users.noreply.github.com>
2021-10-25 19:37:59 -0300

    Correção na Authentication Controller

--- Adapters.Api/Controllers/AutenticacaoController.cs
+++ Adapters.Api/Controllers/AutenticacaoController.cs
@@ -1,6 +1,7 @@
 ﻿using Adapters.Authentication.Security;
 using Clinica.Application.Interfaces;
 using Clinica.Application.Models.Usuario;
+using Clinica.Application.Security.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -16,18 +17,19 @@ namespace Adapters.Api.Controllers
     {
         private readonly IUsuarioApplicationService _application;
         private readonly JwtToken _token;
+        private readonly ICriptoService _cripto;
 
-        public AutenticacaoController(IUsuarioApplicationService application, JwtToken token)
+        public AutenticacaoController(IUsuarioApplicationService application, JwtToken token, ICriptoService cripto)
         {
             _application = application;
             _token = token;
+            _cripto = cripto;
         }
         
         [HttpPost]
         public IActionResult Post(UsuarioAuthenticationModel model)
-        {
-            var usuario = _application.GetByLoginSenha(model.Email, model.Senha);
-            //if (usuario.Login.Equals(model.Email) && usuario.Senha.Equals(model.Senha))
+        {            
+            var usuario = _application.GetByLoginSenha(model.Email, _cripto.Ecrypt(model.Senha));
             if(usuario != null)
             {
                 try
